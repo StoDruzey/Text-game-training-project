55c0f7ec46b534b7712c0b6e9a87827ecd38ad34
Serge Labada <sergelabada@gmail.com>
2022-07-01 00:04:46 +0300

    Study project

--- app/src/main/java/com/example/nyethack/Creature.kt
+++ app/src/main/java/com/example/nyethack/Creature.kt
@@ -8,9 +8,9 @@ interface Fightable {
     val diceCount: Int
     val diceSides: Int
     val damageRoll: Int
-    get() = (0 until diceCount).map {
+        get() = (0 until diceCount).map {
         Random().nextInt(diceSides + 1)
-    }.sum()
+        }.sum()
 
     fun attack(opponent: Fightable): Int
 }
