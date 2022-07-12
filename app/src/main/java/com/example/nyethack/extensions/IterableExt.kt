2102b5d504f87d88dbb532356fd05a5438319a12
Serge Labada <sergelabada@gmail.com>
2022-07-12 19:36:04 +0300

    Study project

--- app/src/main/java/com/example/nyethack/extensions/IterableExt.kt
+++ app/src/main/java/com/example/nyethack/extensions/IterableExt.kt
@@ -10,3 +10,12 @@ fun String.toDragonSpeak() = this.replace(Regex("[AEIOUaeiou]")) {
         else -> it.value
     }
 }
+
+fun frame(name: String, padding: Int, formatChar: String = "*"): String {
+    val greeting = "$name!"
+    val middle = formatChar.padEnd(padding)
+        .plus(greeting)
+        .plus(formatChar.padStart(padding))
+    val end = (0 until middle.length).joinToString("") { formatChar }
+    return "$end\n$middle\n$end"
+}
